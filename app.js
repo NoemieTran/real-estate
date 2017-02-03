21d867f6e1b058bb8bb48ce9b035630267858ea5
Noémie TRAN <Nomie TRAN>
2017-02-03 16:25:39 +0100

    Start TP3

--- app.js
+++ app.js
@@ -13,12 +13,30 @@ app.set( 'view engine', 'ejs' );
 app.use( '/assets', express.static( 'assets' ) );
 
 
+//Mets le résultat dans la console
+function callLeboncoin (){
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s'
+
+    request (url, function (error, response, html)
+    {
+        if (!error && response.statusCode == 200 ) {
+            var $ = cheerio.load (html)
+
+            console.log ($('h2.item_price span_value').text())
+        }
+        else {
+            console.log(error)
+        }
+    })
+}
+
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
     res.render( 'home', {
         message: 'The Home Page!'
     });
 });
+
 
 
 //launch the server on the 3000 port
